61d662e47ea4116accb2d37c52fb0735b245d65d
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-01-27 12:57:35 -0300

    Making layout Header

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { Container, Content, Logo } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <div>
+          <Logo>
+            <strong>OUTSIDE</strong>
+          </Logo>
+          <nav>
+            <NavLink activeStyle={{ color: '#f5f5cc' }} to="/attractions">
+              Atrações
+            </NavLink>
+          </nav>
+        </div>
+        <aside>
+          <strong>Administrador</strong>
+          <span>sair do sistema</span>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ web/src/components/Header/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #bb2222;
+  padding: 0 50px;
+  border-bottom: 3px solid #fff;
+  box-shadow: 0.5px 0.5px 3px 2px rgba(0, 0, 0, 0.2);
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  div {
+    display: flex;
+    align-items: center;
+    a {
+      font-size: 16px;
+      font-weight: bold;
+      margin-right: 24px;
+      color: #f5f5cc;
+    }
+  }
+
+  aside {
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+    strong {
+      font-size: 14px;
+      margin-bottom: 2px;
+      color: #fff;
+    }
+    span {
+      margin-top: 2px;
+      font-size: 12px;
+      color: #f5f5cc;
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Logo = styled.div`
+  align-items: center;
+  padding-right: 30px;
+  margin-right: 30px;
+  border-right: 1px solid #fff;
+
+  strong {
+    color: #fff;
+    font-weight: bold;
+    font-size: 24px;
+  }
+`;
