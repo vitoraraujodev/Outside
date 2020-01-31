e2ea6b7f4cbd3fb22f4dbfff072be39b6f7943cb
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-01-31 17:46:54 -0300

    SignUp page

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,12 +1,68 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import googleLogo from '~/assets/googleLogo.png';
+import facebookLogo from '~/assets/facebookLogo.png';
 
-export default function SignUp() {
+import {
+  LogoText,
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+  AlterCointainer,
+  AlterText,
+  AlterLogin,
+} from './styles';
+
+import Background from '~/components/Background';
+
+export default function SignUp({ navigation }) {
   return (
-    <View>
-      <Text>SignUp Page</Text>
-    </View>
+    <Background>
+      <Container>
+        <LogoText>OUTSIDE</LogoText>
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Confirmação de senha"
+          />
+          <SubmitButton onPress={() => {}}>Cadastrar</SubmitButton>
+        </Form>
+        <SignLink
+          onPress={() => {
+            navigation.navigate('SignIn');
+          }}
+        >
+          <SignLinkText>Já possuo uma conta</SignLinkText>
+        </SignLink>
+        <AlterText>Cadastre também pelo:</AlterText>
+        <AlterCointainer>
+          <AlterLogin source={googleLogo} />
+
+          <AlterLogin source={facebookLogo} />
+        </AlterCointainer>
+      </Container>
+    </Background>
   );
 }
---
+++ mobile/src/pages/SignUp/styles.js
@@ -0,0 +1,70 @@
+import { Platform } from 'react-native';
+import styled from 'styled-components/native';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+export const LogoText = styled.Text`
+  color: #fff;
+  font-size: 44px;
+  font-weight: bold;
+`;
+
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  margin-top: 50px;
+`;
+
+export const Form = styled.View`
+  background: #fff;
+  padding: 28px;
+  border-radius: 4px;
+  align-self: stretch;
+  margin-top: 50px;
+  elevation: 2;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 12px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 12px;
+`;
+
+export const SignLink = styled.TouchableOpacity`
+  margin-top: 12px;
+`;
+
+export const SignLinkText = styled.Text`
+  color: #eee;
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const AlterCointainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 12px;
+`;
+
+export const AlterText = styled.Text`
+  font-size: 18px;
+  color: #fff;
+  font-weight: bold;
+  margin-top: 40px;
+`;
+
+export const AlterLogin = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
+  margin: 0 12px;
+`;
